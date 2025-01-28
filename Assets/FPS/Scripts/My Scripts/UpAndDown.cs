21f6fde0f2adc9dc45accbd6c11428a2a0491d25
Iida Lapinkoski <lapinkoski.iida@gmail.com>
2025-01-28 11:08:31 +0200

    did changes lol

--- Assets/FPS/Scripts/My Scripts/UpAndDown.cs
+++ Assets/FPS/Scripts/My Scripts/UpAndDown.cs
@@ -6,34 +6,30 @@ namespace AG3787
     {
         [SerializeField] private Transform[] targetEndPoints;
 
-        public float speed = (1f);
+        public float Speed = 1f;
 
         private Transform target;
 
         private int currentEndPointIndex = 0;
 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             target = targetEndPoints[0];
         }
 
-        // Update is called once per frame
         void Update()
         {
-            var step = speed* Time.deltaTime;
+            var step = Speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, target.position, step);
 
             if(Vector3.Distance(transform.position, target.position) < 0.001f)
-            {
                 SwapEndPointTarget();
-            }
         }
 
-        public void SwapEndPointTarget()
+        private void SwapEndPointTarget()
         {
             currentEndPointIndex++;
-            currentEndPointIndex%= targetEndPoints.Length;
+            currentEndPointIndex %= targetEndPoints.Length;
             target = targetEndPoints[currentEndPointIndex];
         }
     } 
